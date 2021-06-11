7a77e9ce74efdfc3baab8f8457fc10eca26185ea
Bauchet-Tristan <t.bauchet@rns.ecolescreatives.com>
2021-06-11 16:09:03 +0200

    Update lvl1.js

--- lvl1.js
+++ lvl1.js
@@ -48,7 +48,7 @@ class lvl1 extends Phaser.Scene //
 
 
         //---player--//
-        player = this.physics.add.sprite(600, 0, 'dude').setOrigin(0.5,0.5).setSize(40,85,false);
+        player = this.physics.add.sprite(playerX, playerY, 'dude').setOrigin(0.5,0.5).setSize(40,85,false);
 
         this.physics.world.setBounds(0,0,this.carteDuNiveau.widthInPixels,this.carteDuNiveau.heightInPixels);
 
